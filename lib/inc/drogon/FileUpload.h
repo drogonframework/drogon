10a6d39a325ee22078840ccf2391967c786195e9
an-tao <20741618@qq.com>
2018-06-11 15:31:34 +0800

    add FileUpload,not complete yet

---
+++ lib/inc/drogon/FileUpload.h
@@ -0,0 +1,51 @@
+/**
+ *
+ *  @file
+ *  @author An Tao
+ *  @section LICENSE
+ *
+ *  Copyright 2018, An Tao.  All rights reserved.
+ *  Use of this source code is governed by a MIT license
+ *  that can be found in the License file.
+ *
+ *  @section DESCRIPTION
+ *
+ */
+
+#pragma
+
+#include "HttpRequest.h"
+#include <string>
+#include <map>
+#include <vector>
+namespace drogon
+{
+    class HttpFile
+    {
+    public:
+        const std::string& getFileName() const {return fileName_;};
+        void setFileName(const std::string& filename){fileName_=filename;};
+        void setFile(const std::string& file){fileContent_=file;};
+        int save(const std::string& path);
+        int saveAs(const std::string& filename);
+        int64_t fileLength() const {return fileContent_.length();};
+        const std::string getMd5()const;
+    protected:
+        std::string fileName_;
+        std::string fileContent_;
+    };
+    class FileUpload
+    {
+    public:
+        FileUpload(){};
+        ~FileUpload(){};
+        const std::vector<HttpFile> getFiles();
+        const std::map<std::string,std::string>& getPremeter() const;
+        int parse(const HttpRequest& req);
+
+    protected:
+        std::vector<HttpFile> files_;
+        std::map<std::string,std::string> premeter_;
+        int parseEntity(const std::string& str);
+    };
+}
---
+++ lib/src/FileUpload.cc
@@ -0,0 +1,141 @@
+#include <drogon/FileUpload.h>
+#include "Funcs.h"
+#include <openssl/md5.h>
+#include <iostream>
+#include <fstream>
+using namespace drogon;
+
+const std::vector<HttpFile> FileUpload::getFiles()
+{
+    return files_;
+}
+const std::map<std::string, std::string> &FileUpload::getPremeter() const
+{
+    return premeter_;
+};
+int FileUpload::parse(const HttpRequest &req)
+{
+    if (req.method() != HttpRequest::kPost)
+        return -1;
+    std::map<std::string, std::string> headers = req.headers();
+    if (headers.find("Content-Type") == headers.end())
+        return -1;
+    std::string &contentType = headers["Content-Type"];
+    std::string::size_type pos = contentType.find(";");
+    if (pos == std::string::npos)
+        return -1;
+
+    std::string type = contentType.substr(0, pos);
+    if (type != "multipart/form-data")
+        return -1;
+    pos = contentType.find("boundary=");
+    if (pos == std::string::npos)
+        return -1;
+    std::string boundary = contentType.substr(pos + 9);
+    std::cout << "boundary[" << boundary << "]" << std::endl;
+    std::string content = req.query();
+    std::string::size_type pos1, pos2;
+    pos1 = pos2 = 0;
+    pos2 = content.find(boundary);
+    while (1)
+    {
+        pos1 = pos2;
+        if (pos1 == std::string::npos)
+            break;
+        pos1 += boundary.length();
+        if (content[pos1] == '\r' && content[pos1 + 1] == '\n')
+            pos1 += 2;
+        pos2 = content.find(boundary, pos1);
+        if (pos2 == std::string::npos)
+            break;
+        //    std::cout<<"pos1="<<pos1<<" pos2="<<pos2<<std::endl;
+        if (content[pos2 - 4] == '\r' && content[pos2 - 3] == '\n' && content[pos2 - 2] == '-' && content[pos2 - 1] == '-')
+            pos2 -= 4;
+        if (parseEntity(content.substr(pos1, pos2 - pos1)) != 0)
+            return -1;
+        //pos2+=boundary.length();
+    }
+    return 0;
+}
+int FileUpload::parseEntity(const std::string &str)
+{
+    //    parseEntity:[Content-Disposition: form-data; name="userfile1"; filename="appID.txt"
+    //    Content-Type: text/plain
+    //
+    //    AA004MV7QI]
+    //    pos1=190 pos2=251
+    //    parseEntity:[YvBu
+    //    Content-Disposition: form-data; name="text"
+    //
+    //    text]
+
+    // std::cout<<"parseEntity:["<<str<<"]"<<std::endl;
+    std::string::size_type pos = str.find("name=\"");
+    if (pos == std::string::npos)
+        return -1;
+    pos += 6;
+    std::string::size_type pos1 = str.find("\"", pos);
+    if (pos1 == std::string::npos)
+        return -1;
+    std::string name = str.substr(pos, pos1 - pos);
+    // std::cout<<"name=["<<name<<"]\n";
+    pos = str.find("filename=\"");
+    if (pos == std::string::npos)
+    {
+        pos1 = str.find("\r\n\r\n");
+        if (pos1 == std::string::npos)
+            return -1;
+        premeter_[name] = str.substr(pos1 + 4);
+        return 0;
+    }
+    else
+    {
+        pos += 10;
+        std::string::size_type pos1 = str.find("\"", pos);
+        if (pos1 == std::string::npos)
+            return -1;
+        HttpFile file;
+        file.setFileName(str.substr(pos, pos1 - pos));
+        pos1 = str.find("\r\n\r\n");
+        if (pos1 == std::string::npos)
+            return -1;
+        file.setFile(str.substr(pos1 + 4));
+        files_.push_back(file);
+        return 0;
+    }
+}
+
+int HttpFile::save(const std::string &path)
+{
+    if (fileName_ == "")
+        return -1;
+    std::string filename;
+    if (path[path.length()] != '/')
+    {
+        filename = path + "/" + fileName_;
+    }
+    else
+        filename = path + fileName_;
+    return saveAs(filename);
+}
+int HttpFile::saveAs(const std::string &filename)
+{
+    std::ofstream file(filename);
+    if (file.is_open())
+    {
+        file << fileContent_;
+        file.close();
+        return 0;
+    }
+    else
+        return -1;
+}
+const std::string HttpFile::getMd5() const
+{
+    MD5_CTX c;
+    unsigned char md5[16] = {0};
+    MD5_Init(&c);
+    MD5_Update(&c, fileContent_.c_str(), fileContent_.size());
+    MD5_Final(md5, &c);
+    return StringToHex(md5, 16);
+}
