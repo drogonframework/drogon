fd4f43816a14a6c11205b73c8ad177539ef16e05
an-tao <20741618@qq.com>
2018-06-15 09:44:40 +0800

    modify CMakeLists.txt

--- lib/src/SharedLibManager.cc
+++ lib/src/SharedLibManager.cc
@@ -105,7 +105,7 @@ void SharedLibManager::managerLibs()
                         else
                             return;
                     }
-                    
+
                     {
                         std::ofstream fout(lockFile);
                     }
