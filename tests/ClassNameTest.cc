2fc238aa8023c0893fdbc6ef311a42b67c28e28f
antao <antao@guoxin.cn>
2018-08-08 17:58:43 +0800

    add a testing

---
+++ tests/ClassNameTest.cc
@@ -0,0 +1,25 @@
+#include <drogon/DrObject.h>
+#include <iostream>
+namespace api{
+    namespace v1{
+        template <typename T>
+        class handler:public drogon::DrObject<T>
+        {
+        public:
+            static void p(){
+                std::cout<<handler<T>::classTypeName()<<std::endl;
+            }
+        };
+        class hh:public handler<hh>
+        {
+
+        };
+    }
+}
+int main()
+{
+    api::v1::hh h;
+    std::cout<<h.className()<<std::endl;
+    std::cout<<api::v1::hh::classTypeName()<<std::endl;
+    h.p();
+}
