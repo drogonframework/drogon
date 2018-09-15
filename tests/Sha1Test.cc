649f6f3925fed7692a64ca6d8dc5c8731690e42e
an-tao <antao2002@gmail.com>
2018-09-15 22:04:52 +0800

    1.Use OpenSSL as an optional lib;2.Add SHA1 test

---
+++ tests/Sha1Test.cc
@@ -0,0 +1,15 @@
+#ifdef USE_OPENSSL
+#include <openssl/sha.h>
+#else
+#include "../lib/src/Sha1.h"
+#endif
+#include <stdio.h>
+int main()
+{
+	unsigned char in[]="1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890";
+	unsigned char out[SHA_DIGEST_LENGTH]={0};
+	SHA1(in,100,out);
+	for(int i=0;i<SHA_DIGEST_LENGTH;i++)
+		printf("%02x",out[i]);
+	putchar('\n');
+}
