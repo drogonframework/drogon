2c9f461bc2c22d1d4ba5dea0d41b99f33d10a359
juzi <juzi@gmail.com>
2019-03-22 08:56:02 +0800

    Fixed coding bug

--- lib/src/Utilities.cc
+++ lib/src/Utilities.cc
@@ -714,17 +714,18 @@ namespace drogon
         }
 
         int charCount(const std::string& str) {
-            int c, i, ix, q;
-            for (q = 0, i = 0, ix = str.length(); i < ix; i++, q++) {
-                c = (unsigned char) str[i];
-                if (c >= 0   && c <= 127) {
-                    i += 0;
+            int length = str.length();
+            int ch, index, length, count;
+            for (count = 0, index = 0; index < length; index++, count++) {
+                ch = (unsigned char) str[i];
+                if (ch <= 127) {
+                    index += 0;
                 } else if ((c & 0xE0) == 0xC0) {
-                    i += 1;
+                    index += 1;
                 } else if ((c & 0xF0) == 0xE0) {
-                    i += 2;
+                    index += 2;
                 } else if ((c & 0xF8) == 0xF0) {
-                    i += 3;
+                    index += 3;
                 }
                 //else if (($c & 0xFC) == 0xF8) i+=4; // 111110bb //byte 5, unnecessary in 4 byte UTF-8
                 //else if (($c & 0xFE) == 0xFC) i+=5; // 1111110b //byte 6, unnecessary in 4 byte UTF-8
@@ -732,7 +733,7 @@ namespace drogon
                     return 0; 
                 } 
             }
-            return q;
+            return count;
         }
 
     } // namespace utils
