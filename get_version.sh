705250eb90092359e06d6fa11352fa61962fd487
antao <antao2002@gmail.com>
2019-04-07 01:25:38 +0800

    Update version to 0.9.32

--- get_version.sh
+++ get_version.sh
@@ -3,7 +3,7 @@
 GIT_VER=$(git log|grep ^commit|wc -l|sed -e "s/^ *//")
 MD5=$(git log|head -1|awk '{printf $2}')
 TMP_FILE=/tmp/version
-echo "#define VERSION \"0.9.31.$GIT_VER\"" > ${TMP_FILE}
+echo "#define VERSION \"0.9.32.$GIT_VER\"" > ${TMP_FILE}
 echo "#define VERSION_MD5 \"$MD5\"" >> ${TMP_FILE}
 if [ ! -f $1 ];then
 mv -f ${TMP_FILE} $1
