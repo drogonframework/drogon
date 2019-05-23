3452a2fa1cffaca13463c1bbfd7545c20cdef5a9
antao <antao2002@gmail.com>
2019-05-23 18:42:49 +0800

    Modify some code to get a good style

--- lib/src/ssl_funcs/Md5.cc
+++ lib/src/ssl_funcs/Md5.cc
@@ -22,79 +22,75 @@ const int Md5Encode::kB = 0xefcdab89;
 const int Md5Encode::kC = 0x98badcfe;
 const int Md5Encode::kD = 0x10325476;
 
-const unsigned long long Md5Encode::k_ti_num_integer = 4294967296;
+const unsigned long long Md5Encode::tiNumInteger = 4294967296;
 
 // function: CycleMoveLeft
-// @param src_num: the number to be moved left
-// @param bit_num_to_move: the number of bit of moving
+// @param srcNum: the number to be moved left
+// @param bitNumToMove: the number of bit of moving
 // @return  result after moving
-UInt32 Md5Encode::CycleMoveLeft(UInt32 src_num, int bit_num_to_move)
+uint32_t Md5Encode::cycleMoveLeft(uint32_t srcNum, int bitNumToMove)
 {
-    UInt32 src_num1 = src_num;
-    UInt32 src_num2 = src_num;
-    if (0 >= bit_num_to_move)
+    uint32_t srcNum1 = srcNum;
+    uint32_t srcNum2 = srcNum;
+    if (0 >= bitNumToMove)
     {
-        return src_num;
+        return srcNum;
     }
-    //    UInt32 num1 = src_num1 << bit_num_to_move;
-    //    UInt32 num2 = src_num2 >> (32 - bit_num_to_move);
-
-    return ((src_num1 << bit_num_to_move) |
-            (src_num2 >> (32 - bit_num_to_move)));
+    return ((srcNum1 << bitNumToMove) | (srcNum2 >> (32 - bitNumToMove)));
 }
 
 // function: FillData
-// @param in_data_ptr:    input data
-// @param data_byte_len:  length of input data
-// @param out_data_ptr:   output data
+// @param inDataPtr:    input data
+// @param dataByteLen:  length of input data
+// @param outDataPtr:   output data
 // return : length of output data
-UInt32 Md5Encode::FillData(const char *in_data_ptr,
-                           int data_byte_len,
-                           char **out_data_ptr)
+uint32_t Md5Encode::fillData(const char *inDataPtr,
+                             int dataByteLen,
+                             char **outDataPtr)
 {
-    int bit_num = data_byte_len * BIT_OF_BYTE;
-    // int grop_num = bit_num / BIT_OF_GROUP;
-    int mod_bit_num = bit_num % BIT_OF_GROUP;
-    int bit_need_fill = 0;
-    if (mod_bit_num > (BIT_OF_GROUP - SRC_DATA_LEN))
+    int bitNum = dataByteLen * BIT_OF_BYTE;
+    // int grop_num = bitNum / BIT_OF_GROUP;
+    int modBitNum = bitNum % BIT_OF_GROUP;
+    int bitNeedFill = 0;
+    if (modBitNum > (BIT_OF_GROUP - SRC_DATA_LEN))
     {
-        bit_need_fill = (BIT_OF_GROUP - mod_bit_num);
-        bit_need_fill += (BIT_OF_GROUP - SRC_DATA_LEN);
+        bitNeedFill = (BIT_OF_GROUP - modBitNum);
+        bitNeedFill += (BIT_OF_GROUP - SRC_DATA_LEN);
     }
     else
     {
-        bit_need_fill = (BIT_OF_GROUP - SRC_DATA_LEN) - mod_bit_num;
+        bitNeedFill = (BIT_OF_GROUP - SRC_DATA_LEN) - modBitNum;
     }
-    int all_bit = bit_num + bit_need_fill;
-    if (0 < bit_need_fill)
+    int allBit = bitNum + bitNeedFill;
+    if (0 < bitNeedFill)
     {
-        *out_data_ptr =
-            new char[all_bit / BIT_OF_BYTE + SRC_DATA_LEN / BIT_OF_BYTE];
-        memset(*out_data_ptr,
+        *outDataPtr =
+            new char[allBit / BIT_OF_BYTE + SRC_DATA_LEN / BIT_OF_BYTE];
+        memset(*outDataPtr,
                0,
-               all_bit / BIT_OF_BYTE + SRC_DATA_LEN / BIT_OF_BYTE);
+               allBit / BIT_OF_BYTE + SRC_DATA_LEN / BIT_OF_BYTE);
         // copy data
-        memcpy(*out_data_ptr, in_data_ptr, data_byte_len);
+        memcpy(*outDataPtr, inDataPtr, dataByteLen);
         // fill rest data
-        unsigned char *tmp = reinterpret_cast<unsigned char *>(*out_data_ptr);
-        tmp += data_byte_len;
+        unsigned char *tmp = reinterpret_cast<unsigned char *>(*outDataPtr);
+        tmp += dataByteLen;
         // fill 1 and 0
         *tmp = 0x80;
         // fill origin data len
-        unsigned long long *origin_num =
-            (unsigned long long *)((*out_data_ptr) + ((all_bit / BIT_OF_BYTE)));
-        *origin_num = data_byte_len * BIT_OF_BYTE;
+        unsigned long long *originNum =
+            (unsigned long long *)((*outDataPtr) + ((allBit / BIT_OF_BYTE)));
+        *originNum = dataByteLen * BIT_OF_BYTE;
     }
-    return (all_bit / BIT_OF_BYTE + SRC_DATA_LEN / BIT_OF_BYTE);
+    return (allBit / BIT_OF_BYTE + SRC_DATA_LEN / BIT_OF_BYTE);
 }
 
-void Md5Encode::RoundF(char *data_BIT_OF_GROUP_ptr, ParamDynamic &param)
+void Md5Encode::roundF(char *data512Ptr, ParamDynamic &param)
 {
-    UInt32 *M = reinterpret_cast<UInt32 *>(data_BIT_OF_GROUP_ptr);
+    uint32_t *M = reinterpret_cast<uint32_t *>(data512Ptr);
     int s[] = {7, 12, 17, 22};
     for (int i = 0; i < 16; ++i)
     {
-        UInt32 ti = k_ti_num_integer * abs(sin(i + 1));
+        uint32_t ti = tiNumInteger * abs(sin(i + 1));
         if (i % 4 == 0)
         {
             FF(param.ua_, param.ub_, param.uc_, param.ud_, M[i], s[i % 4], ti);
@@ -114,13 +110,13 @@ void Md5Encode::RoundF(char *data_BIT_OF_GROUP_ptr, ParamDynamic &param)
     }
 }
 
-void Md5Encode::RoundG(char *data_BIT_OF_GROUP_ptr, ParamDynamic &param)
+void Md5Encode::roundG(char *data512Ptr, ParamDynamic &param)
 {
-    UInt32 *M = reinterpret_cast<UInt32 *>(data_BIT_OF_GROUP_ptr);
+    uint32_t *M = reinterpret_cast<uint32_t *>(data512Ptr);
     int s[] = {5, 9, 14, 20};
     for (int i = 0; i < 16; ++i)
     {
-        UInt32 ti = k_ti_num_integer * abs(sin(i + 1 + 16));
+        uint32_t ti = tiNumInteger * abs(sin(i + 1 + 16));
         int index = (i * 5 + 1) % 16;
         if (i % 4 == 0)
         {
@@ -165,13 +161,13 @@ void Md5Encode::RoundG(char *data_BIT_OF_GROUP_ptr, ParamDynamic &param)
     }
 }
 
-void Md5Encode::RoundH(char *data_BIT_OF_GROUP_ptr, ParamDynamic &param)
+void Md5Encode::roundH(char *data512Ptr, ParamDynamic &param)
 {
-    UInt32 *M = reinterpret_cast<UInt32 *>(data_BIT_OF_GROUP_ptr);
+    uint32_t *M = reinterpret_cast<uint32_t *>(data512Ptr);
     int s[] = {4, 11, 16, 23};
     for (int i = 0; i < 16; ++i)
     {
-        UInt32 ti = k_ti_num_integer * abs(sin(i + 1 + 32));
+        uint32_t ti = tiNumInteger * abs(sin(i + 1 + 32));
         int index = (i * 3 + 5) % 16;
         if (i % 4 == 0)
         {
@@ -216,13 +212,13 @@ void Md5Encode::RoundH(char *data_BIT_OF_GROUP_ptr, ParamDynamic &param)
     }
 }
 
-void Md5Encode::RoundI(char *data_BIT_OF_GROUP_ptr, ParamDynamic &param)
+void Md5Encode::roundI(char *data512Ptr, ParamDynamic &param)
 {
-    UInt32 *M = reinterpret_cast<UInt32 *>(data_BIT_OF_GROUP_ptr);
+    uint32_t *M = reinterpret_cast<uint32_t *>(data512Ptr);
     int s[] = {6, 10, 15, 21};
     for (int i = 0; i < 16; ++i)
     {
-        UInt32 ti = k_ti_num_integer * abs(sin(i + 1 + 48));
+        uint32_t ti = tiNumInteger * abs(sin(i + 1 + 48));
         int index = (i * 7 + 0) % 16;
         if (i % 4 == 0)
         {
@@ -267,16 +263,16 @@ void Md5Encode::RoundI(char *data_BIT_OF_GROUP_ptr, ParamDynamic &param)
     }
 }
 
-void Md5Encode::RotationCalculate(char *data_512_ptr, ParamDynamic &param)
+void Md5Encode::rotationCalculate(char *data512Ptr, ParamDynamic &param)
 {
-    if (NULL == data_512_ptr)
+    if (NULL == data512Ptr)
     {
         return;
     }
-    RoundF(data_512_ptr, param);
-    RoundG(data_512_ptr, param);
-    RoundH(data_512_ptr, param);
-    RoundI(data_512_ptr, param);
+    roundF(data512Ptr, param);
+    roundG(data512Ptr, param);
+    roundH(data512Ptr, param);
+    roundI(data512Ptr, param);
     param.ua_ = param.va_last_ + param.ua_;
     param.ub_ = param.vb_last_ + param.ub_;
     param.uc_ = param.vc_last_ + param.uc_;
@@ -289,7 +285,7 @@ void Md5Encode::RotationCalculate(char *data_512_ptr, ParamDynamic &param)
 }
 
 // Convert to hex format string
-std::string Md5Encode::GetHexStr(unsigned int num_str)
+std::string Md5Encode::getHexStr(unsigned int numStr)
 {
     std::string hexstr = "";
     char szch[] = {'0',
@@ -308,8 +304,8 @@ std::string Md5Encode::GetHexStr(unsigned int num_str)
                    'D',
                    'E',
                    'F'};
-    unsigned char *tmptr = (unsigned char *)&num_str;
-    int len = sizeof(num_str);
+    unsigned char *tmptr = (unsigned char *)&numStr;
+    int len = sizeof(numStr);
     for (int i = 0; i < len; i++)
     {
         unsigned char ch = tmptr[i] & 0xF0;
@@ -322,9 +318,9 @@ std::string Md5Encode::GetHexStr(unsigned int num_str)
 }
 
 // function: Encode
-// @param src_info: the string to be encoded.
+// @param srcInfo: the string to be encoded.
 // return : the string after encoding
-std::string Md5Encode::encode(const std::string &src_info)
+std::string Md5Encode::encode(const std::string &srcInfo)
 {
     ParamDynamic param;
     param.ua_ = kA;
@@ -337,20 +333,19 @@ std::string Md5Encode::encode(const std::string &src_info)
     param.vd_last_ = kD;
 
     std::string result;
-    char *out_data_ptr = NULL;
-    int total_byte =
-        FillData(src_info.c_str(), src_info.length(), &out_data_ptr);
-    // char * data_BIT_OF_GROUP = out_data_ptr;
-    for (int i = 0; i < total_byte / (BIT_OF_GROUP / BIT_OF_BYTE); ++i)
+    char *outDataPtr = NULL;
+    int totalByte = fillData(srcInfo.c_str(), srcInfo.length(), &outDataPtr);
+    // char * dataBitOfGroup = outDataPtr;
+    for (int i = 0; i < totalByte / (BIT_OF_GROUP / BIT_OF_BYTE); ++i)
     {
-        char *data_BIT_OF_GROUP = out_data_ptr;
-        data_BIT_OF_GROUP += i * (BIT_OF_GROUP / BIT_OF_BYTE);
-        RotationCalculate(data_BIT_OF_GROUP, param);
+        char *dataBitOfGroup = outDataPtr;
+        dataBitOfGroup += i * (BIT_OF_GROUP / BIT_OF_BYTE);
+        rotationCalculate(dataBitOfGroup, param);
     }
-    delete[] out_data_ptr, out_data_ptr = NULL;
-    result.append(GetHexStr(param.ua_));
-    result.append(GetHexStr(param.ub_));
-    result.append(GetHexStr(param.uc_));
-    result.append(GetHexStr(param.ud_));
+    delete[] outDataPtr, outDataPtr = NULL;
+    result.append(getHexStr(param.ua_));
+    result.append(getHexStr(param.ub_));
+    result.append(getHexStr(param.uc_));
+    result.append(getHexStr(param.ud_));
     return result;
 }
