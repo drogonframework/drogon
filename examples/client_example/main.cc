5cea136ea447bfe65b20ab517b6e3c946a9c7d46
an-tao <antao2002@gmail.com>
2020-01-20 12:58:49 +0800

    Works fine on VS2019

--- examples/client_example/main.cc
+++ examples/client_example/main.cc
@@ -9,7 +9,7 @@ int main()
     trantor::Logger::setLogLevel(trantor::Logger::kTrace);
     {
         int count = 0;
-        auto client = HttpClient::newHttpClient("http://www.baidu.com");
+        auto client = HttpClient::newHttpClient("https://www.baidu.com");
         auto req = HttpRequest::newHttpRequest();
         req->setMethod(drogon::Get);
         req->setPath("/s");
