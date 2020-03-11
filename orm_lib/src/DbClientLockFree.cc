bb8b5ded79ad14abe1adc7645d1ab31b1ecf0090
An Tao <antao2002@gmail.com>
2020-03-11 12:41:20 +0800

    Fix a fatal bug in the MysqlConnection class (#379)

--- orm_lib/src/DbClientLockFree.cc
+++ orm_lib/src/DbClientLockFree.cc
@@ -52,19 +52,12 @@ DbClientLockFree::DbClientLockFree(const std::string &connInfo,
 {
     type_ = type;
     LOG_TRACE << "type=" << (int)type;
-    if (type == ClientType::PostgreSQL)
+    if (type == ClientType::PostgreSQL || type == ClientType::Mysql)
     {
         loop_->queueInLoop([this]() {
             for (size_t i = 0; i < connectionsNumber_; ++i)
                 connectionHolders_.push_back(newConnection());
         });
-    }
-    else if (type == ClientType::Mysql)
-    {
-        for (size_t i = 0; i < connectionsNumber_; ++i)
-            loop_->runAfter(0.1 * (i + 1), [this]() {
-                connectionHolders_.push_back(newConnection());
-            });
     }
     else
     {
@@ -231,6 +224,9 @@ std::shared_ptr<Transaction> DbClientLockFree::newTransaction(
     const std::function<void(bool)> &)
 {
     // Don't support transaction;
+    LOG_ERROR
+        << "You can't use the synchronous interface in the fast Database "
+           "client, please use the asynchronous version (newTransactionAsync)";
     assert(0);
     return nullptr;
 }
@@ -324,7 +320,8 @@ void DbClientLockFree::handleNewTask(const DbConnectionPtr &conn)
 #if LIBPQ_SUPPORTS_BATCH_MODE
         if (type_ != ClientType::PostgreSQL)
         {
-            auto &cmd = sqlCmdBuffer_.front();
+            std::shared_ptr<SqlCmd> cmd = std::move(sqlCmdBuffer_.front());
+            sqlCmdBuffer_.pop_front();
             conn->execSql(std::move(cmd->sql_),
                           cmd->parametersNumber_,
                           std::move(cmd->parameters_),
@@ -332,7 +329,6 @@ void DbClientLockFree::handleNewTask(const DbConnectionPtr &conn)
                           std::move(cmd->formats_),
                           std::move(cmd->callback_),
                           std::move(cmd->exceptionCallback_));
-            sqlCmdBuffer_.pop_front();
         }
         else
         {
@@ -342,7 +338,8 @@ void DbClientLockFree::handleNewTask(const DbConnectionPtr &conn)
             conn->batchSql(std::move(cmds));
         }
 #else
-        auto &cmd = sqlCmdBuffer_.front();
+        std::shared_ptr<SqlCmd> cmd = std::move(sqlCmdBuffer_.front());
+        sqlCmdBuffer_.pop_front();
         conn->execSql(std::move(cmd->sql_),
                       cmd->parametersNumber_,
                       std::move(cmd->parameters_),
@@ -350,7 +347,6 @@ void DbClientLockFree::handleNewTask(const DbConnectionPtr &conn)
                       std::move(cmd->formats_),
                       std::move(cmd->callback_),
                       std::move(cmd->exceptionCallback_));
-        sqlCmdBuffer_.pop_front();
 #endif
         return;
     }
