b1502557f6809c864a079429dac19302f1807156
antao <antao2002@gmail.com>
2020-03-18 17:30:40 +0800

    Update trantor

--- cmake_modules/FindJsoncpp.cmake
+++ cmake_modules/FindJsoncpp.cmake
@@ -7,45 +7,37 @@
 #
 # This module defines JSONCPP_INCLUDE_DIRS, where to find header, etc.
 # JSONCPP_LIBRARIES, the libraries needed to use jsoncpp. JSONCPP_FOUND, If
-# false, do not try to use jsoncpp. JSONCPP_INCLUDE_PREFIX, include prefix for
-# jsoncpp
+# false, do not try to use jsoncpp. 
+# Jsoncpp_lib - The imported target library.
 
 # only look in default directories
-find_path(JSONCPP_INCLUDE_DIR
+find_path(JSONCPP_INCLUDE_DIRS
           NAMES json/json.h
           DOC "jsoncpp include dir"
           PATH_SUFFIXES jsoncpp)
 
-find_library(JSONCPP_LIBRARY NAMES jsoncpp DOC "jsoncpp library")
-
-set(JSONCPP_INCLUDE_DIRS ${JSONCPP_INCLUDE_DIR})
-set(JSONCPP_LIBRARIES ${JSONCPP_LIBRARY})
+find_library(JSONCPP_LIBRARIES NAMES jsoncpp DOC "jsoncpp library")
 
 # debug library on windows same naming convention as in qt (appending debug
 # library with d) boost is using the same "hack" as us with "optimized" and
-# "debug"
-#if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
-#  find_library(JSONCPP_LIBRARY_DEBUG NAMES jsoncppd DOC "jsoncpp debug library")
-#  if("${JSONCPP_LIBRARY_DEBUG}" STREQUAL "JSONCPP_LIBRARY_DEBUG-NOTFOUND")
-#    set(JSONCPP_LIBRARY_DEBUG ${JSONCPP_LIBRARIES})
-#  endif()
+# "debug" if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
+# find_library(JSONCPP_LIBRARIES_DEBUG NAMES jsoncppd DOC "jsoncpp debug
+# library") if("${JSONCPP_LIBRARIES_DEBUG}" STREQUAL "JSONCPP_LIBRARIES_DEBUG-
+# NOTFOUND") set(JSONCPP_LIBRARIES_DEBUG ${JSONCPP_LIBRARIES}) endif()
 
-#  set(JSONCPP_LIBRARIES
-#      optimized
-#      ${JSONCPP_LIBRARIES}
-#      debug
-#      ${JSONCPP_LIBRARY_DEBUG})
+# set(JSONCPP_LIBRARIES optimized ${JSONCPP_LIBRARIES} debug
+# ${JSONCPP_LIBRARIES_DEBUG})
 
-#endif()
+# endif()
 
 # handle the QUIETLY and REQUIRED arguments and set JSONCPP_FOUND to TRUE if all
 # listed variables are TRUE, hide their existence from configuration view
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Jsoncpp
                                   DEFAULT_MSG
-                                  JSONCPP_INCLUDE_DIR
-                                  JSONCPP_LIBRARY)
-mark_as_advanced(JSONCPP_INCLUDE_DIR JSONCPP_LIBRARY)
+                                  JSONCPP_INCLUDE_DIRS
+                                  JSONCPP_LIBRARIES)
+mark_as_advanced(JSONCPP_INCLUDE_DIRS JSONCPP_LIBRARIES)
 
 if(Jsoncpp_FOUND)
   if(NOT EXISTS ${JSONCPP_INCLUDE_DIRS}/json/version.h)
@@ -69,8 +61,8 @@ if(Jsoncpp_FOUND)
   add_library(Jsoncpp_lib INTERFACE IMPORTED)
   set_target_properties(Jsoncpp_lib
                         PROPERTIES INTERFACE_INCLUDE_DIRECTORIES
-                                   ${JSONCPP_INCLUDE_DIR}
+                                   "${JSONCPP_INCLUDE_DIRS}"
                                    INTERFACE_LINK_LIBRARIES
-                                   ${JSONCPP_LIBRARY})
+                                   "${JSONCPP_LIBRARIES}")
 
 endif(Jsoncpp_FOUND)
--- cmake_modules/FindSQLite3.cmake
+++ cmake_modules/FindSQLite3.cmake
@@ -8,33 +8,33 @@
 
 # * Find sqlite3 Find the native SQLITE3 headers and libraries.
 #
-# SQLITE3_INCLUDE_DIRS    - where to find sqlite3.h, etc. SQLITE3_LIBRARIES -
-# List of libraries when using sqlite. SQLite3_FOUND   - True if sqlite found.
+# SQLITE3_INCLUDE_DIRS    - where to find sqlite3.h, etc. 
+# SQLITE3_LIBRARIES - List of libraries when using sqlite. 
+# SQLite3_FOUND   - True if sqlite3 found.
+# SQLite3_lib - The imported target library.
 
 # Look for the header file.
-find_path(SQLITE3_INCLUDE_DIR NAMES sqlite3.h)
+find_path(SQLITE3_INCLUDE_DIRS NAMES sqlite3.h)
 
 # Look for the library.
-find_library(SQLITE3_LIBRARY NAMES sqlite3)
+find_library(SQLITE3_LIBRARIES NAMES sqlite3)
 
 # Handle the QUIETLY and REQUIRED arguments and set SQLite3_FOUND to TRUE if all
 # listed variables are TRUE.
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(SQLite3
                                   DEFAULT_MSG
-                                  SQLITE3_LIBRARY
-                                  SQLITE3_INCLUDE_DIR)
+                                  SQLITE3_LIBRARIES
+                                  SQLITE3_INCLUDE_DIRS)
 
 # Copy the results to the output variables.
 if(SQLite3_FOUND)
-  set(SQLITE3_LIBRARIES ${SQLITE3_LIBRARY})
-  set(SQLITE3_INCLUDE_DIRS ${SQLITE3_INCLUDE_DIR})
   add_library(SQLite3_lib INTERFACE IMPORTED)
   set_target_properties(SQLite3_lib
                         PROPERTIES INTERFACE_INCLUDE_DIRECTORIES
-                                   ${SQLITE3_INCLUDE_DIR}
+                                   "${SQLITE3_INCLUDE_DIRS}"
                                    INTERFACE_LINK_LIBRARIES
-                                   ${SQLITE3_LIBRARY})
+                                   "${SQLITE3_LIBRARIES}")
 else(SQLite3_FOUND)
   set(SQLITE3_LIBRARIES)
   set(SQLITE3_INCLUDE_DIRS)
