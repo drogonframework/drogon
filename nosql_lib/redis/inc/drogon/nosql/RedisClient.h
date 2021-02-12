f747e32b166d4b6e3f5764d80d34cc16675dad9f
an-tao <antao2002@gmail.com>
2021-02-12 13:05:27 +0800

    Redis init

---
+++ nosql_lib/redis/inc/drogon/nosql/RedisClient.h
@@ -0,0 +1,24 @@
+/**
+ *
+ *  @file RedisClient.h
+ *  @author An Tao
+ *
+ *  Copyright 2018, An Tao.  All rights reserved.
+ *  https://github.com/an-tao/drogon
+ *  Use of this source code is governed by a MIT license
+ *  that can be found in the License file.
+ *
+ *  Drogon
+ *
+ */
+#pragma once
+
+namespace drogon
+{
+namespace nosql
+{
+class RedisClient
+{
+};
+}  // namespace nosql
+}  // namespace drogon
---
+++ nosql_lib/redis/inc/drogon/nosql/RedisResult.h
@@ -0,0 +1,25 @@
+/**
+ *
+ *  @file RedisResult.h
+ *  @author An Tao
+ *
+ *  Copyright 2018, An Tao.  All rights reserved.
+ *  https://github.com/an-tao/drogon
+ *  Use of this source code is governed by a MIT license
+ *  that can be found in the License file.
+ *
+ *  Drogon
+ *
+ */
+
+#pragma once
+
+namespace drogon
+{
+namespace nosql
+{
+class RedisResult
+{
+};
+}  // namespace nosql
+}  // namespace drogon
