ae7451dc703fe153abaf67e75c3ca7ed20c2e98b
Juergen Gleiss <office@gleiss.at>
2021-03-12 10:11:26 +0100

    Merge feature/ORM_convertMethod

--- cmake/DrogonUtilities.cmake
+++ cmake/DrogonUtilities.cmake
@@ -58,3 +58,62 @@ function(drogon_create_views arg)
   endforeach()
   target_sources(${ARGV0} PRIVATE ${VIEWSRC})
 endfunction(drogon_create_views)
+
+# ##############################################################################
+# function drogon_create_models(model_path)
+# ##############################################################################
+function(drogon_create_models arg)
+  if(ARGC LESS 1)
+    message(STATUS "arguments error when calling drogon_create_model")
+    return()
+  endif()
+  file(GLOB_RECURSE SCP_LIST ${ARGV1}/*.csp)
+  foreach(cspFile ${SCP_LIST})
+    file(RELATIVE_PATH
+         inFile
+         ${CMAKE_CURRENT_SOURCE_DIR}
+         ${cspFile})
+    if(ARGC GREATER 3 AND ARGV3)
+      string(REPLACE "/"
+                     "_"
+                     f1
+                     ${inFile})
+      string(REPLACE "\\"
+                     "_"
+                     f2
+                     ${f1})
+      string(REPLACE ".csp"
+                     ""
+                     outputFile
+                     ${f2})
+      add_custom_command(OUTPUT ${ARGV2}/${outputFile}.h ${ARGV2}/${outputFile}.cc
+                         COMMAND drogon_ctl
+                                 ARGS
+                                 create
+                                 view
+                                 ${inFile}
+                                 --path-to-namespace
+                                 -o
+                                 ${ARGV2}
+                         DEPENDS ${cspFile}
+                         WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+                         VERBATIM)
+      set(VIEWSRC ${VIEWSRC} ${ARGV2}/${outputFile}.cc)
+    else()
+      get_filename_component(classname ${cspFile} NAME_WE)
+      add_custom_command(OUTPUT ${ARGV2}/${classname}.h ${ARGV2}/${classname}.cc
+                         COMMAND drogon_ctl
+                                 ARGS
+                                 create
+                                 view
+                                 ${inFile}
+                                 -o
+                                 ${ARGV2}
+                         DEPENDS ${cspFile}
+                         WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+                         VERBATIM)
+      set(VIEWSRC ${VIEWSRC} ${ARGV2}/${classname}.cc)
+    endif()
+  endforeach()
+  target_sources(${ARGV0} PRIVATE ${VIEWSRC})
+endfunction(drogon_create_models)
