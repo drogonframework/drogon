drogonframework/drogon

commit 54727a5dbe61d8faf2845621dc80992a17f75a21

Add exports macro to allow Shared Library with hidden symbols by default (e.g. Windows) (#745)
Co-authored-by: an-tao <antao2002@gmail.com>

lib/inc/drogon/IntranetIpFilter.h
@@ -1,7 +1,7 @@
 /**
  *
- *  IntranetIpFilter.h
- *  An Tao
+ *  @file IntranetIpFilter.h
+ *  @author An Tao
  *
  *  Copyright 2018, An Tao.  All rights reserved.
  *  https://github.com/an-tao/drogon
@@ -14,6 +14,7 @@
 
 #pragma once
 
+#include <drogon/exports.h>
 #include <drogon/HttpFilter.h>
 
 namespace drogon
@@ -21,12 +22,10 @@ namespace drogon
 /**
  * @brief A filter that prohibit access from external networks
  */
-class IntranetIpFilter : public HttpFilter<IntranetIpFilter>
+class DROGON_EXPORT IntranetIpFilter : public HttpFilter<IntranetIpFilter>
 {
   public:
-    IntranetIpFilter()
-    {
-    }
+    IntranetIpFilter();
     virtual void doFilter(const HttpRequestPtr &req,
                           FilterCallback &&fcb,
                           FilterChainCallback &&fccb) override;

lib/inc/drogon/LocalHostFilter.h
@@ -1,7 +1,7 @@
 /**
  *
- *  LocalHostFilter.h
- *  An Tao
+ *  @file LocalHostFilter.h
+ *  @author An Tao
  *
  *  Copyright 2018, An Tao.  All rights reserved.
  *  https://github.com/an-tao/drogon
@@ -14,6 +14,7 @@
 
 #pragma once
 
+#include <drogon/exports.h>
 #include <drogon/HttpFilter.h>
 
 namespace drogon
@@ -21,12 +22,10 @@ namespace drogon
 /**
  * @brief A filter that prohibit access from other hosts.
  */
-class LocalHostFilter : public HttpFilter<LocalHostFilter>
+class DROGON_EXPORT LocalHostFilter : public HttpFilter<LocalHostFilter>
 {
   public:
-    LocalHostFilter()
-    {
-    }
+    LocalHostFilter();
     virtual void doFilter(const HttpRequestPtr &req,
                           FilterCallback &&fcb,
                           FilterChainCallback &&fccb) override;

lib/inc/drogon/NotFound.h
@@ -2,8 +2,8 @@
 
 /**
  *
- *  NotFound.h
- *  An Tao
+ *  @file NotFound.h
+ *  @author An Tao
  *
  *  Copyright 2018, An Tao.  All rights reserved.
  *  https://github.com/an-tao/drogon
@@ -13,7 +13,9 @@
  *  Drogon
  *
  */
+#pragma once
 
+#include <drogon/exports.h>
 #include <drogon/DrTemplate.h>
 namespace drogon
 {
@@ -21,11 +23,10 @@ namespace drogon
  * @brief This class is used by the drogon to generate the 404 page. Users don't
  * use this class directly.
  */
-class NotFound : public drogon::DrTemplate<NotFound>
+class DROGON_EXPORT NotFound : public drogon::DrTemplate<NotFound>
 {
   public:
-    NotFound(){};
-    virtual ~NotFound(){};
+    NotFound();
     virtual std::string genText(const drogon::HttpViewData &) override;
 };
 }  // namespace drogon

lib/inc/drogon/plugins/SecureSSLRedirector.h
@@ -1,10 +1,11 @@
 /**
  *
- *  drogon_plugin_SecureSSLRedirector.h
+ *  @file drogon_plugin_SecureSSLRedirector.h
  *
  */
 
 #pragma once
+#include <drogon/exports.h>
 #include <drogon/drogon_callbacks.h>
 #include <drogon/plugins/Plugin.h>
 #include <regex>
@@ -40,12 +41,11 @@ namespace plugin
  * configuration file.
  *
  */
-class SecureSSLRedirector : public drogon::Plugin<SecureSSLRedirector>
+class DROGON_EXPORT SecureSSLRedirector
+    : public drogon::Plugin<SecureSSLRedirector>
 {
   public:
-    SecureSSLRedirector()
-    {
-    }
+    SecureSSLRedirector();
     /// This method must be called by drogon to initialize and start the plugin.
     /// It must be implemented by the user.
     virtual void initAndStart(const Json::Value &config) override;

lib/src/IntranetIpFilter.cc
@@ -27,3 +27,4 @@ void IntranetIpFilter::doFilter(const HttpRequestPtr &req,
     auto res = drogon::HttpResponse::newNotFoundResponse();
     fcb(res);
 }
+IntranetIpFilter::IntranetIpFilter() = default;

lib/src/LocalHostFilter.cc
@@ -27,3 +27,4 @@ void LocalHostFilter::doFilter(const HttpRequestPtr &req,
     auto res = drogon::HttpResponse::newNotFoundResponse();
     fcb(res);
 }
+LocalHostFilter::LocalHostFilter() = default;

lib/src/NotFound.cc
@@ -49,3 +49,4 @@ std::string NotFound::genText(const HttpViewData &NotFound_view_data)
                            "error page -->\n";
     return NotFound_tmp_stream.str();
 }
+NotFound::NotFound() = default;

lib/src/SecureSSLRedirector.cc
@@ -96,3 +96,4 @@ HttpResponsePtr SecureSSLRedirector::redirectToSSL(
         }
     }
 }
+SecureSSLRedirector::SecureSSLRedirector() = default;

commit cbf461fb8cb7cb7c688533d43a0856d9292afa31

Fix a bug of reflection failure introduced by #745

lib/inc/drogon/IntranetIpFilter.h
@@ -25,7 +25,7 @@ namespace drogon
 class DROGON_EXPORT IntranetIpFilter : public HttpFilter<IntranetIpFilter>
 {
   public:
-    IntranetIpFilter();
+    IntranetIpFilter(){}
     virtual void doFilter(const HttpRequestPtr &req,
                           FilterCallback &&fcb,
                           FilterChainCallback &&fccb) override;

lib/inc/drogon/LocalHostFilter.h
@@ -25,7 +25,7 @@ namespace drogon
 class DROGON_EXPORT LocalHostFilter : public HttpFilter<LocalHostFilter>
 {
   public:
-    LocalHostFilter();
+    LocalHostFilter(){}
     virtual void doFilter(const HttpRequestPtr &req,
                           FilterCallback &&fcb,
                           FilterChainCallback &&fccb) override;

lib/inc/drogon/NotFound.h
@@ -23,10 +23,10 @@ namespace drogon
  * @brief This class is used by the drogon to generate the 404 page. Users don't
  * use this class directly.
  */
-class DROGON_EXPORT NotFound : public drogon::DrTemplate<NotFound>
+class DROGON_EXPORT NotFound final : public drogon::DrTemplate<NotFound>
 {
   public:
-    NotFound();
+    NotFound(){}
     virtual std::string genText(const drogon::HttpViewData &) override;
 };
 }  // namespace drogon

lib/inc/drogon/plugins/SecureSSLRedirector.h
@@ -45,7 +45,7 @@ class DROGON_EXPORT SecureSSLRedirector
     : public drogon::Plugin<SecureSSLRedirector>
 {
   public:
-    SecureSSLRedirector();
+    SecureSSLRedirector(){}
     /// This method must be called by drogon to initialize and start the plugin.
     /// It must be implemented by the user.
     virtual void initAndStart(const Json::Value &config) override;

lib/src/IntranetIpFilter.cc
@@ -27,4 +27,3 @@ void IntranetIpFilter::doFilter(const HttpRequestPtr &req,
     auto res = drogon::HttpResponse::newNotFoundResponse();
     fcb(res);
 }
-IntranetIpFilter::IntranetIpFilter() = default;

lib/src/LocalHostFilter.cc
@@ -27,4 +27,3 @@ void LocalHostFilter::doFilter(const HttpRequestPtr &req,
     auto res = drogon::HttpResponse::newNotFoundResponse();
     fcb(res);
 }
-LocalHostFilter::LocalHostFilter() = default;

lib/src/NotFound.cc
@@ -49,4 +49,3 @@ std::string NotFound::genText(const HttpViewData &NotFound_view_data)
                            "error page -->\n";
     return NotFound_tmp_stream.str();
 }
-NotFound::NotFound() = default;

lib/src/SecureSSLRedirector.cc
@@ -96,4 +96,3 @@ HttpResponsePtr SecureSSLRedirector::redirectToSSL(
         }
     }
 }
-SecureSSLRedirector::SecureSSLRedirector() = default;