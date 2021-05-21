035de87ff07ff935e2e53a299f7d5019f8400ca5
an-tao <antao2002@gmail.com>
2021-05-21 23:51:19 +0800

    Use reference

--- orm_lib/inc/drogon/orm/DbClient.h
+++ orm_lib/inc/drogon/orm/DbClient.h
@@ -54,11 +54,13 @@ struct SqlAwaiter : public CallbackAwaiter<Result>
 
     void await_suspend(std::coroutine_handle<> handle)
     {
-        binder_ >> [handle, this](const drogon::orm::Result &result) {
+        binder_ >> [handle, this](const drogon::orm::Result &result)
+        {
             setValue(result);
             handle.resume();
         };
-        binder_ >> [handle, this](const std::exception_ptr &e) {
+        binder_ >> [handle, this](const std::exception_ptr &e)
+        {
             setException(e);
             handle.resume();
         };
@@ -174,8 +176,8 @@ class DROGON_EXPORT DbClient : public trantor::NonCopyable
         std::shared_ptr<std::promise<Result> > prom =
             std::make_shared<std::promise<Result> >();
         binder >> [prom](const Result &r) { prom->set_value(r); };
-        binder >>
-            [prom](const std::exception_ptr &e) { prom->set_exception(e); };
+        binder >> [prom](const std::exception_ptr &e)
+        { prom->set_exception(e); };
         binder.exec();
         return prom->get_future();
     }
@@ -201,8 +203,8 @@ class DROGON_EXPORT DbClient : public trantor::NonCopyable
 
 #ifdef __cpp_impl_coroutine
     template <typename... Arguments>
-    internal::SqlAwaiter execSqlCoro(const std::string sql,
-                                     Arguments... args) noexcept
+    internal::SqlAwaiter execSqlCoro(const std::string &sql,
+                                     Arguments &&...args) noexcept
     {
         auto binder = *this << sql;
         (void)std::initializer_list<int>{
@@ -320,7 +322,8 @@ inline void internal::TrasactionAwaiter::await_suspend(
 {
     assert(client_ != nullptr);
     client_->newTransactionAsync(
-        [this, handle](const std::shared_ptr<Transaction> &transaction) {
+        [this, handle](const std::shared_ptr<Transaction> &transaction)
+        {
             if (transaction == nullptr)
                 setException(std::make_exception_ptr(TimeoutError(
                     "Timeout, no connection available for transaction")));
