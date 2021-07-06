b018b30ea4ad197fc7bfaab4b15f816d73c98cea
Sebastián González <brutalchrist@gmail.com>
2021-07-06 09:19:11 -0400

    Delete config.json

--- examples/redis/main.cc
+++ examples/redis/main.cc
@@ -5,8 +5,6 @@ int main()
 {
     // Set HTTP listener address and port
     drogon::app().addListener("0.0.0.0", 8080);
-    // Load config file
-    // drogon::app().loadConfigFile("../config.json");
     // Run HTTP framework,the method will block in the internal event loop
     drogon::app().createRedisClient("127.0.0.1", 6379);
     drogon::app().run();
