ababfaae9175ba58c35cea1d83334056fdcd0f5a
an-tao <antao2002@gmail.com>
2021-07-06 13:17:33 +0800

    clang-format

--- examples/redis/controllers/Client.h
+++ examples/redis/controllers/Client.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <drogon/HttpController.h>
 using namespace drogon;
-class Client: public drogon::HttpController<Client>
+class Client : public drogon::HttpController<Client>
 {
   public:
     METHOD_LIST_BEGIN
@@ -9,6 +9,10 @@ class Client: public drogon::HttpController<Client>
     METHOD_ADD(Client::post, "{1}", Post);
     METHOD_LIST_END
 
-    void get(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback, std::string key);
-    void post(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback, std::string key);
+    void get(const HttpRequestPtr &req,
+             std::function<void(const HttpResponsePtr &)> &&callback,
+             std::string key);
+    void post(const HttpRequestPtr &req,
+              std::function<void(const HttpResponsePtr &)> &&callback,
+              std::string key);
 };
--- examples/redis/main.cc
+++ examples/redis/main.cc
@@ -1,13 +1,14 @@
 #include <drogon/drogon.h>
 #include <drogon/nosql/RedisClient.h>
 
-int main() {
-  //Set HTTP listener address and port
-  drogon::app().addListener("0.0.0.0",8080);
-  //Load config file
-  //drogon::app().loadConfigFile("../config.json");
-  //Run HTTP framework,the method will block in the internal event loop
-  drogon::app().createRedisClient("127.0.0.1", 6379);
-  drogon::app().run();
-  return 0;
+int main()
+{
+    // Set HTTP listener address and port
+    drogon::app().addListener("0.0.0.0", 8080);
+    // Load config file
+    // drogon::app().loadConfigFile("../config.json");
+    // Run HTTP framework,the method will block in the internal event loop
+    drogon::app().createRedisClient("127.0.0.1", 6379);
+    drogon::app().run();
+    return 0;
 }
