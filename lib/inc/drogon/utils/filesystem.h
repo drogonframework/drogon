cd7aefad356cbb74c0053f65950b0c844963985e
Christophe Greisberger <christophe@greisberger.fr>
2021-07-08 20:57:00 +0200

    Removed cmake generation of drogon/utils/filesystem.h

---
+++ lib/inc/drogon/utils/filesystem.h
@@ -0,0 +1,53 @@
+/*  @author An Tao
+ *
+ *  Copyright 2018, An Tao.  All rights reserved.
+ *  https://github.com/an-tao/drogon
+ *  Use of this source code is governed by a MIT license
+ *  that can be found in the License file.
+ *
+ *  Drogon
+ *
+ */
+
+#pragma once
+
+/* Check of std::filesystem::path availability: 
+ * - OS X > 10.14.x Mojave (latest __cplusplus is 201707)
+ * - Windows: Visual Studio >= 2019 (c++20)
+ * - Others: should already have it in c++17
+ */
+#if (defined(__APPLE__) && __cplusplus > 201707L) || \
+    (defined(_WIN32) && defined(_MSC_VER) && _MSC_VER >= 1920) || \
+    (!defined(__APPLE__) && !defined(_WIN32) && __cplusplus >= 201703L)
+#define HAS_STD_FILESYSTEM_PATH
+#endif
+
+#ifdef HAS_STD_FILESYSTEM_PATH
+#include <filesystem>
+#else
+#include <boost/filesystem.hpp>
+#endif
+
+namespace drogon {
+#ifdef HAS_STD_FILESYSTEM_PATH
+    namespace filesystem = std::filesystem;
+#else
+    namespace filesystem = boost::filesystem;
+#endif
+}   // namespace drogon
+
+namespace trantor
+{
+inline LogStream &operator<<(LogStream &ls, const drogon::filesystem::path &v)
+{
+#if defined(_WIN32) && defined(__cpp_char8_t)
+    // Convert UCS-2 to UTF-8, not ASCII - not needed on other OSes
+    auto u8path{ v.u8string() };
+    ls.append((const char*)u8path.data(), u8path.length());
+#else
+    // No need to convert
+    ls.append(v.string().data(), v.string().length());
+#endif
+    return ls;
+}
+}   // namespace trantor
