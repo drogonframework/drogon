66878d2318a8e74fed7f39c3ab30847f5ca06bfc
an-tao <antao2002@gmail.com>
2021-08-15 22:43:18 +0800

    Parsing header works

--- drogon_ctl/create_swagger.cc
+++ drogon_ctl/create_swagger.cc
@@ -32,7 +32,202 @@
 #include <regex>
 
 using namespace drogon_ctl;
+namespace internal
+{
+class StructNode;
+using StructNodePtr = std::shared_ptr<StructNode>;
+class StructNode
+{
+  public:
+    enum NodeType
+    {
+        kRoot = 0,
+        kClass,
+        kNameSpace
+    };
 
+    StructNode(const std::string &content,
+               const std::string &name,
+               NodeType type = kRoot)
+        : type_(type), name_(name), content_(content)
+    {
+        LOG_DEBUG << "new node:" << name << "-" << type;
+        if (type != kClass)
+            children_ = parse(content);
+    }
+    const std::string &content() const
+    {
+        return content_;
+    }
+    const std::string &name() const
+    {
+        return name_;
+    }
+    NodeType type() const
+    {
+        return type_;
+    }
+    void print() const
+    {
+        print(0);
+    }
+
+  private:
+    std::vector<StructNodePtr> children_;
+    std::string content_;
+    NodeType type_;
+    std::string name_;
+    std::pair<std::string, std::string> findContentOfClassOrNameSpace(
+        const std::string &content,
+        std::string::size_type start)
+    {
+        int braces = 0;
+        std::string::size_type pos1{start};
+        std::string::size_type pos2{content.size() - 1};
+        for (auto i = start; i < content.size(); i++)
+        {
+            if (content[i] == '{')
+            {
+                braces++;
+                if (braces == 1)
+                {
+                    pos1 = i + 1;
+                }
+            }
+            else if (content[i] == '}')
+            {
+                braces--;
+                if (braces == 0)
+                {
+                    pos2 = i;
+                    break;
+                }
+            }
+        }
+        return {content.substr(pos1, pos2 - pos1), content.substr(pos2 + 1)};
+    }
+    std::pair<StructNodePtr, std::string> findClass(const std::string &content)
+    {
+        LOG_DEBUG << "findClass: " << content;
+        if (content.empty())
+            return {nullptr, ""};
+        std::regex rx(R"(class[ \r\n]+([^ \r\n]+)[ \r\n]+)");
+        std::smatch results;
+        if (std::regex_search(content, results, rx))
+        {
+            assert(results.size() > 1);
+            auto nextPart =
+                findContentOfClassOrNameSpace(content, results.position());
+            return {std::make_shared<StructNode>(nextPart.first,
+                                                 results[1].str(),
+                                                 kClass),
+                    nextPart.second};
+        }
+        return {nullptr, ""};
+    }
+    std::tuple<std::string, StructNodePtr, std::string> findNameSpace(
+        const std::string &content)
+    {
+        LOG_DEBUG << "findNameSpace";
+        if (content.empty())
+            return {"", nullptr, ""};
+        std::regex rx(R"(namespace[ \r\n]+([^ \r\n]+)[ \r\n]*\{)");
+        std::smatch results;
+        if (std::regex_search(content, results, rx))
+        {
+            assert(results.size() > 1);
+            auto pos = results.position();
+            auto first = content.substr(0, pos);
+            auto nextPart = findContentOfClassOrNameSpace(content, pos);
+            auto npNodePtr = std::make_shared<StructNode>(nextPart.first,
+                                                          results[1].str(),
+                                                          kNameSpace);
+
+            return {first, npNodePtr, nextPart.second};
+        }
+        else
+        {
+            return {"", nullptr, ""};
+        }
+    }
+    std::vector<StructNodePtr> parse(const std::string &content)
+    {
+        std::vector<StructNodePtr> res;
+        auto t = findNameSpace(content);
+        if (std::get<1>(t))
+        {
+            res.emplace_back(std::get<1>(t));
+            auto firstPart = std::get<0>(t);
+            while (1)
+            {
+                auto p = findClass(firstPart);
+                if (p.first)
+                {
+                    res.emplace_back(p.first);
+                    firstPart = p.second;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            auto subsequentNode = parse(std::get<2>(t));
+            for (auto &node : subsequentNode)
+            {
+                res.emplace_back(node);
+            }
+            return res;
+        }
+        std::string classPart = content;
+        while (1)
+        {
+            auto p = findClass(classPart);
+            if (p.first)
+            {
+                res.emplace_back(p.first);
+                classPart = p.second;
+            }
+            else
+            {
+                break;
+            }
+        }
+        return res;
+    }
+    void print(int indent) const
+    {
+        std::string ind(indent, ' ');
+        std::cout << ind;
+        switch (type_)
+        {
+            case kRoot:
+            {
+                std::cout << "Root\n" << ind << "{\n";
+                break;
+            }
+            case kClass:
+            {
+                std::cout << "class " << name_ << "\n" << ind << "{\n";
+                break;
+            }
+            case kNameSpace:
+            {
+                std::cout << "namespace " << name_ << "\n" << ind << "{\n";
+                break;
+            }
+        }
+
+        for (auto child : children_)
+        {
+            child->print(indent + 2);
+        }
+        std::cout << ind << "}";
+        if (type_ == kClass)
+            std::cout << ";";
+        std::cout << "\n";
+    }
+};
+}  // namespace internal
 static void forEachControllerHeaderIn(
     std::string strPath,
     const std::function<void(const std::string &)> &cb)
@@ -95,17 +290,8 @@ static void parseControllerHeader(const std::string &headerFile,
     pbuf->sgetn(&fileContent[0], filesize);
     std::cout << fileContent;
 
-    std::regex rx(R"(namespace[ \r\n]+([^ \r\n]+)[ \r\n]*\{)");
-    std::smatch results;
-    while (std::regex_search(fileContent, results, rx))
-    {
-        if (results.size() > 1)
-        {
-            for (int i = 1; i < results.size(); i++)
-                std::cout << "nnn:" << i << results[i] << std::endl;
-        }
-        fileContent = results.suffix();
-    }
+    internal::StructNode r(fileContent, "root", internal::StructNode::kRoot);
+    r.print();
 }
 static std::string makeSwaggerDocument(const Json::Value &config)
 {
