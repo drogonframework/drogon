4c151e610cb9e0b1d8ff6023313f9617ee3444a7
an-tao <antao2002@gmail.com>
2021-11-29 00:28:04 +0800

    flag

--- lib/src/HttpFileImpl.cc
+++ lib/src/HttpFileImpl.cc
@@ -84,7 +84,8 @@ int HttpFileImpl::saveTo(const filesystem::path &pathAndFileName) const
 {
     LOG_TRACE << "save uploaded file:" << pathAndFileName;
     auto wPath = utils::toNativePath(pathAndFileName.native());
-    std::ofstream file(wPath, std::ios::binary);
+    std::ofstream file(wPath,
+                       std::ios::binary | std::ios::out | std::ios::trunc);
     if (file.is_open())
     {
         file.write(fileContent_.data(), fileContent_.size());
