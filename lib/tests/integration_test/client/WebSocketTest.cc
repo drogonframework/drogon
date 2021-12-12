6f19a604428a8342752d6e44e1779447a2b2d3ab
marty1885 <marty188586@gmail.com>
2021-12-12 09:29:09 +0800

    fix WS test potentially can get stuck

--- lib/tests/integration_test/client/WebSocketTest.cc
+++ lib/tests/integration_test/client/WebSocketTest.cc
@@ -7,35 +7,46 @@
 using namespace drogon;
 using namespace std::chrono_literals;
 
+struct DataPack
+{
+    WebSocketClientPtr wsPtr;
+    std::shared_ptr<drogon::test::CaseBase> TEST_CTX;
+};
+
 static WebSocketClientPtr wsPtr_;
 DROGON_TEST(WebSocketTest)
 {
     wsPtr_ = WebSocketClient::newWebSocketClient("127.0.0.1", 8848);
+    auto pack = std::make_shared<DataPack>({wsPtr_, TEST_CTX});
     auto req = HttpRequest::newHttpRequest();
     req->setPath("/chat");
     wsPtr_->setMessageHandler(
-        [TEST_CTX](const std::string &message,
+        [pack](const std::string &message,
                    const WebSocketClientPtr &wsPtr,
                    const WebSocketMessageType &type) mutable {
-            CHECK((type == WebSocketMessageType::Text ||
-                   type == WebSocketMessageType::Pong));
-            if (type == WebSocketMessageType::Pong)
+            CHECK(type == WebSocketMessageType::Text ||
+                   type == WebSocketMessageType::Pong);
+            if (type == WebSocketMessageType::Pong && pack != nullptr)
             {
+                auto TEST_CTX = pack->TEST_CTX;
+                auto wsPtr = pack->wsPtr;
+
                 wsPtr_->stop();
                 CHECK(message.empty());
-                TEST_CTX = {};
+                pack.reset();
             }
         });
 
     wsPtr_->connectToServer(
         req,
-        [TEST_CTX](ReqResult r,
+        [TEST_CTX, pack](ReqResult r,
                    const HttpResponsePtr &resp,
                    const WebSocketClientPtr &wsPtr) mutable {
             if (r != ReqResult::Ok)
             {
                 wsPtr_->stop();
                 wsPtr_.reset();
+                pack.reset();
             }
             REQUIRE(r == ReqResult::Ok);
             REQUIRE(wsPtr != nullptr);
