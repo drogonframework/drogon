3b0fd3d676040fc8715d710bc85feb659b948c45
Robert Huang <56649783@qq.com>
2022-05-17 13:20:05 +0800

    Fix HttpFile unittest error on Windows (#1191)

--- lib/tests/unittests/HttpFileTest.cc
+++ lib/tests/unittests/HttpFileTest.cc
@@ -7,7 +7,20 @@ using namespace std;
 
 DROGON_TEST(HttpFile)
 {
-    SUBSECTION(Save)
+    SUBSECTION(SavePathUsingDefaultConfigPath)
+    {
+        HttpFileImpl file;
+        file.setFileName("test_file_name");
+        file.setFile("test", 4);
+        auto out = file.save();
+
+        CHECK(out == 0);
+        CHECK(filesystem::exists("./uploads/test_file_name"));
+
+        filesystem::remove_all("./uploads/test_file_name");
+    }
+
+    SUBSECTION(SavePathWithSpecificRelativePath)
     {
         HttpFileImpl file;
         file.setFileName("test_file_name");
@@ -20,7 +33,22 @@ DROGON_TEST(HttpFile)
         filesystem::remove_all("./test_uploads_dir");
     }
 
-    SUBSECTION(SavePathRelativeTraversal)
+    SUBSECTION(SavePathWithSpecificAbsolutePath)
+    {
+        auto uploadPath = filesystem::current_path() / "test_uploads_dir";
+
+        HttpFileImpl file;
+        file.setFileName("test_file_name");
+        file.setFile("test", 4);
+        auto out = file.save(uploadPath.string());
+
+        CHECK(out == 0);
+        CHECK(filesystem::exists(uploadPath / "test_file_name"));
+
+        filesystem::remove_all(uploadPath.string());
+    }
+
+    SUBSECTION(FileNameWithRelativePath)
     {
         auto uploadPath = filesystem::current_path() / "test_uploads_dir";
 
@@ -36,17 +64,19 @@ DROGON_TEST(HttpFile)
         filesystem::remove(uploadPath / "../test_malicious_file_name");
     }
 
-    SUBSECTION(SavePathAbsoluteTraversal)
+    SUBSECTION(FileNameWithAbsolutePath)
     {
+        auto fileName = filesystem::current_path() / "test_malicious_file_name";
+
         HttpFileImpl file;
-        file.setFileName("/tmp/test_malicious_file_name");
+        file.setFileName(fileName.string());
         file.setFile("test", 4);
         auto out = file.save("./test_uploads_dir");
 
         CHECK(out == -1);
-        CHECK(!filesystem::exists("/tmp/test_malicious_file_name"));
+        CHECK(!filesystem::exists(fileName.string()));
 
         filesystem::remove_all("test_uploads_dir");
-        filesystem::remove_all("/tmp/test_malicious_file_name");
+        filesystem::remove(fileName.string());
     }
 }
