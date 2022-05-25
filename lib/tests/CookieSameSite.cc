235339711b399f1e1f816aa5b5cc1e7be6510f72
an-tao <antao2002@gmail.com>
2022-05-26 00:16:38 +0800

    un...

--- lib/tests/CookieSameSite.cc
+++ lib/tests/CookieSameSite.cc
@@ -50,7 +50,7 @@ DROGON_TEST(CookieSameSite)
 
         client->sendRequest(
             req,
-            [TEST_CTX, seq, &p1](ReqResult res, const HttpResponsePtr &resp) {
+            [TEST_CTX, &seq, &p1](ReqResult res, const HttpResponsePtr &resp) {
                 REQUIRE(res == ReqResult::Ok);
                 REQUIRE(resp != nullptr);
 
