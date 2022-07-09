002ed994473265194160b3f2d63029b5f604b75a
An Tao <antao2002@gmail.com>
2022-07-10 00:56:55 +0800

    Update create_model.cc

--- drogon_ctl/create_model.cc
+++ drogon_ctl/create_model.cc
@@ -740,7 +740,8 @@ void create_model::createModelClassFromSqlite3(
     }
     else if (pkNames.size() > 1)
     {
-        for (auto &col : cols) {
+        for (auto &col : cols) 
+        {
             col.isAutoVal_ = false;
         }
 
