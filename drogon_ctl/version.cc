b5388f98aaeb9c053c05d777fe160051e25e0828
Martin Chang <marty1885@users.noreply.github.com>
2023-02-11 22:36:59 +0800

    Fix drogon_ctl build

--- drogon_ctl/version.cc
+++ drogon_ctl/version.cc
@@ -15,6 +15,7 @@
 #include "version.h"
 #include <drogon/config.h>
 #include <drogon/version.h>
+#include <drogon/utils/Utilities.h>
 #include <trantor/net/Resolver.h>
 #include <trantor/utils/Utilities.h>
 #include <iostream>
@@ -32,7 +33,7 @@ static const char banner[] =
 void version::handleCommand(std::vector<std::string> &parameters)
 {
     const auto tlsBackend = trantor::utils::tlsBackend();
-    const bool tlsSupported = utils::supportsTls();
+    const bool tlsSupported = drogon::utils::supportsTls();
     std::cout << banner << std::endl;
     std::cout << "A utility for drogon" << std::endl;
     std::cout << "Version: " << DROGON_VERSION << std::endl;
