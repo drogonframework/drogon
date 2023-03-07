b739a7fab8966c72048d56507c86d22cd6c1d3dc
An Tao <antao2002@gmail.com>
2023-03-07 17:01:23 +0800

    Update ConfigLoader.cc (#1524)

--- lib/src/ConfigLoader.cc
+++ lib/src/ConfigLoader.cc
@@ -378,7 +378,7 @@ static void loadApp(const Json::Value &app)
         }
     }
 #endif
-    auto stackLimit = app.get("json_parser_stack_limit", 0).asUInt64();
+    auto stackLimit = app.get("json_parser_stack_limit", 1000).asUInt64();
     drogon::app().setJsonParserStackLimit(stackLimit);
     auto unicodeEscaping =
         app.get("enable_unicode_escaping_in_json", true).asBool();
