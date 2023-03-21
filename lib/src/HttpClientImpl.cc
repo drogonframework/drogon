4f066258f27dcfe0f3c9ab63a675a4cdb15c98ca
Nitromelon <hwc14@qq.com>
2023-03-21 14:51:37 +0800

    Temporary fix dead HttpClient. (#1542)

--- lib/src/HttpClientImpl.cc
+++ lib/src/HttpClientImpl.cc
@@ -89,6 +89,9 @@ void HttpClientImpl::createTcpClient()
                     }
                     auto resp = responseParser->responseImpl();
                     responseParser->reset();
+                    // temporary fix of dead tcpClientPtr_
+                    // TODO: fix HttpResponseParser when content-length absence
+                    thisPtr->tcpClientPtr_.reset();
                     thisPtr->handleResponse(resp, std::move(firstReq), connPtr);
                     if (!thisPtr->requestsBuffer_.empty())
                     {
