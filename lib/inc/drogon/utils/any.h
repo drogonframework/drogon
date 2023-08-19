b5f8bbb4d85738759f945b12a6801b6b515553e6
Nikolay Mitev <github@hmel.org>
2023-08-19 14:20:28 +0300

    Remove boost::any

--- lib/inc/drogon/utils/any.h
+++ lib/inc/drogon/utils/any.h
@@ -13,19 +13,10 @@
  */
 
 #pragma once
-#if __cplusplus >= 201703L || (defined(_MSVC_LANG) && _MSVC_LANG >= 201703L)
 #include <any>
-#else
-#include <boost/any.hpp>
-#endif
 
 namespace drogon
 {
-#if __cplusplus >= 201703L || (defined(_MSVC_LANG) && _MSVC_LANG >= 201703L)
 using std::any;
 using std::any_cast;
-#else
-using boost::any;
-using boost::any_cast;
-#endif
 }  // namespace drogon
