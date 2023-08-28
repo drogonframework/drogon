b625b7112e23219a48a764c291bd297f3f0b777b
He, Wanchen <hwc14@qq.com>
2023-08-28 20:57:24 +0800

    Fix some typos.

--- lib/tests/unittests/CoroutineTest.cc
+++ lib/tests/unittests/CoroutineTest.cc
@@ -69,7 +69,7 @@ DROGON_TEST(CroutineBasics)
     }());
     CHECK(n == 1);
 
-    // Testing that exceptions can propergate through coroutines
+    // Testing that exceptions can property through coroutines
     auto throw_in_task = [TEST_CTX]() -> Task<> {
         auto f = []() -> Task<> { throw std::runtime_error("test error"); };
 
@@ -77,7 +77,7 @@ DROGON_TEST(CroutineBasics)
     };
     sync_wait(throw_in_task());
 
-    // Test sync_wait propergrates exception
+    // Test sync_wait propagates exception
     auto throws = []() -> Task<> {
         throw std::runtime_error("bla");
         co_return;
@@ -100,7 +100,7 @@ DROGON_TEST(CroutineBasics)
     };
     sync_wait(await_non_copyable());
 
-    // This only works because async_run tries to run the corouine as soon as
+    // This only works because async_run tries to run the coroutine as soon as
     // possible and the coroutine does not wait
     int testVar = 0;
     async_run([&testVar]() -> Task<void> {
