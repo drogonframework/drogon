9fea4ac2318951f68571c7380f20af096ac12d3e
He, Wanchen <hwc14@qq.com>
2023-09-06 12:09:46 +0800

    Update RedisClientSkipped.

--- lib/src/RedisClientSkipped.cc
+++ lib/src/RedisClientSkipped.cc
@@ -18,16 +18,14 @@ namespace drogon
 {
 namespace nosql
 {
-std::shared_ptr<RedisClient> RedisClient::newRedisClient(
-    const trantor::InetAddress& /*serverAddress*/,
-    size_t /*numberOfConnections*/,
-    const std::string& /*password*/,
-    const unsigned int /*db*/,
-    const std::string& /*username*/)
+
+std::shared_ptr<RedisClient> newRedisClient(RedisConnectionInfo,
+                                            size_t /*numberOfConnections*/)
 {
     LOG_FATAL << "Redis is not supported by drogon, please install the "
                  "hiredis library first.";
     abort();
 }
+
 }  // namespace nosql
 }  // namespace drogon
