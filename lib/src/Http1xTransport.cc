9d69aa70fcf02ce61214a0c42247c37eee4bd356
Martin Chang <marty1885@users.noreply.github.com>
2023-11-22 16:15:35 +0800

    isolate HTTP1 transport for client

---
+++ lib/src/Http1xTransport.cc
@@ -0,0 +1,104 @@
+#include "Http1xTransport.h"
+#include "HttpResponseParser.h"
+
+using namespace drogon;
+
+Http1xTransport::Http1xTransport(trantor::TcpConnectionPtr connPtr,
+                                 Version version,
+                                 size_t *bytesSent,
+                                 size_t *bytesReceived)
+    : connPtr(connPtr),
+      bytesSent_(bytesSent),
+      bytesReceived_(bytesReceived),
+      version_(version)
+{
+    connPtr->setContext(std::make_shared<HttpResponseParser>(connPtr));
+}
+
+void Http1xTransport::sendRequestInLoop(const HttpRequestPtr &req,
+                                        HttpReqCallback &&callback)
+{
+    sendReq(req);
+    pipeliningCallbacks_.emplace(std::move(req), std::move(callback));
+}
+
+void Http1xTransport::onRecvMessage(const trantor::TcpConnectionPtr &conn,
+                                    trantor::MsgBuffer *msg)
+{
+    auto responseParser = connPtr->getContext<HttpResponseParser>();
+    assert(responseParser != nullptr);
+    assert(connPtr.get() == conn.get());
+
+    // LOG_TRACE << "###:" << msg->readableBytes();
+    auto msgSize = msg->readableBytes();
+    while (msg->readableBytes() > 0)
+    {
+        if (pipeliningCallbacks_.empty())
+        {
+            LOG_ERROR << "More responses than expected!";
+            connPtr->shutdown();
+            return;
+        }
+        auto &firstReq = pipeliningCallbacks_.front();
+        if (firstReq.first->method() == Head)
+        {
+            responseParser->setForHeadMethod();
+        }
+        if (!responseParser->parseResponse(msg))
+        {
+            *bytesReceived_ += (msgSize - msg->readableBytes());
+            errorCallback(ReqResult::BadResponse);
+            return;
+        }
+        if (responseParser->gotAll())
+        {
+            auto resp = responseParser->responseImpl();
+            responseParser->reset();
+            *bytesReceived_ += (msgSize - msg->readableBytes());
+            msgSize = msg->readableBytes();
+            respCallback(resp, std::move(firstReq), conn);
+
+            pipeliningCallbacks_.pop();
+        }
+        else
+        {
+            break;
+        }
+    }
+}
+
+Http1xTransport::~Http1xTransport()
+{
+}
+
+bool Http1xTransport::handleConnectionClose()
+{
+    auto responseParser = connPtr->getContext<HttpResponseParser>();
+    if (responseParser && responseParser->parseResponseOnClose() &&
+        responseParser->gotAll())
+    {
+        auto &firstReq = pipeliningCallbacks_.front();
+        if (firstReq.first->method() == Head)
+        {
+            responseParser->setForHeadMethod();
+        }
+        auto resp = responseParser->responseImpl();
+        responseParser->reset();
+        respCallback(resp, std::move(firstReq), connPtr);
+        return false;
+    }
+    return true;
+}
+
+void Http1xTransport::sendReq(const HttpRequestPtr &req)
+{
+    trantor::MsgBuffer buffer;
+    assert(req);
+    auto implPtr = static_cast<HttpRequestImpl *>(req.get());
+    assert(version_ == Version::kHttp10 || version_ == Version::kHttp11);
+    implPtr->appendToBuffer(&buffer, version_);
+    LOG_TRACE << "Send request:"
+              << std::string_view(buffer.peek(), buffer.readableBytes());
+    *bytesSent_ += buffer.readableBytes();
+    connPtr->send(std::move(buffer));
+}
--- lib/src/HttpClientImpl.h
+++ lib/src/HttpClientImpl.h
@@ -26,49 +26,10 @@
 #include "impl_forwards.h"
 #include "Http2Transport.h"
 #include "HttpTransport.h"
+#include "Http1xTransport.h"
 
 namespace drogon
 {
-
-class Http1xTransport : public HttpTransport
-{
-  private:
-    std::queue<std::pair<HttpRequestPtr, HttpReqCallback>> pipeliningCallbacks_;
-    trantor::TcpConnectionPtr connPtr;
-    size_t *bytesSent_;
-    size_t *bytesReceived_;
-    Version version_{Version::kHttp11};
-
-    void sendReq(const HttpRequestPtr &req);
-
-  public:
-    Http1xTransport(trantor::TcpConnectionPtr connPtr,
-                    Version version,
-                    size_t *bytesSent,
-                    size_t *bytesReceived);
-    virtual ~Http1xTransport();
-    void sendRequestInLoop(const HttpRequestPtr &req,
-                           HttpReqCallback &&callback) override;
-    void onRecvMessage(const trantor::TcpConnectionPtr &,
-                       trantor::MsgBuffer *) override;
-
-    size_t requestsInFlight() const override
-    {
-        return pipeliningCallbacks_.size();
-    }
-
-    bool handleConnectionClose() override;
-
-    void onError(ReqResult result) override
-    {
-        while (!pipeliningCallbacks_.empty())
-        {
-            auto &cb = pipeliningCallbacks_.front().second;
-            cb(result, nullptr);
-            pipeliningCallbacks_.pop();
-        }
-    }
-};
 
 class HttpClientImpl final : public HttpClient,
                              public std::enable_shared_from_this<HttpClientImpl>
