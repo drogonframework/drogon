93ceb01b59fe815b4dbf1e76c58505e771a9eab3
Martin Chang <marty1885@users.noreply.github.com>
2023-11-23 23:36:09 +0800

    format

--- lib/src/Http2Transport.cc
+++ lib/src/Http2Transport.cc
@@ -681,7 +681,7 @@ void Http2Transport::sendRequestInLoop(const HttpRequestPtr &req,
     vec.emplace_back(":method", req->methodString());
     vec.emplace_back(":path", req->path());
     std::string scheme = connPtr->isSSLConnection() ? "https" : "http";
-    if(!static_cast<drogon::HttpRequestImpl *>(req.get())->passThrough())
+    if (!static_cast<drogon::HttpRequestImpl *>(req.get())->passThrough())
         scheme = (req->isOnSecureConnection()) ? "https" : "http";
     vec.emplace_back(":scheme", scheme);
     if (auto host = req->getHeader("host"); !host.empty())
