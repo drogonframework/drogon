6aed658fabc252eeedc29175fc63212c4e930364
Muhammad <73486659+Mis1eader-dev@users.noreply.github.com>
2024-02-04 16:32:12 +0300

    Change `drogon::MultiPartParser`'s parameters data type from `std::map` to `std::unordered_map` (#1946)

--- lib/src/MultiPart.cc
+++ lib/src/MultiPart.cc
@@ -45,7 +45,9 @@ std::unordered_map<std::string, HttpFile> MultiPartParser::getFilesMap() const
     return result;
 }
 
-const std::map<std::string, std::string> &MultiPartParser::getParameters() const
+const std::
+    unordered_map<std::string, std::string, utils::internal::SafeStringHash> &
+    MultiPartParser::getParameters() const
 {
     return parameters_;
 }
