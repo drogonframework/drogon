8ff81f271d4fea61985560ee55b52c03a7b2c0d4
Lectem <lectem@gmail.com>
2026-05-16 13:23:35 +0200

    Fix ConfigLoader bytesSize failing to parse value ending by 6

--- lib/src/ConfigLoader.cc
+++ lib/src/ConfigLoader.cc
@@ -81,6 +81,7 @@ static bool bytesSize(std::string &sizeStr, size_t &size)
             case '3':
             case '4':
             case '5':
+            case '6':
             case '7':
             case '8':
             case '9':
